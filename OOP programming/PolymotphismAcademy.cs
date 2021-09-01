2074c288f1a70ec4b098dcd305ae6e49da7274df
Amir Ibragimov <ibragimov-amir@list.ru>
2021-09-02 00:15:16 +0500

    solved problem polymorphism

--- OOP programming/PolymotphismAcademy.cs
+++ OOP programming/PolymotphismAcademy.cs
@@ -58,6 +58,89 @@ namespace OOP_programming
             }
             h.Show();
 
+
+            Console.WriteLine("Введите номер ключа pro или exp \nЕсли Хотите воспользоваться беслатной версией просто нажмите на Enter");
+            string key = Console.ReadLine();
+            DocumentWorker document = null;
+
+            switch (key)
+            {
+                case "pro":
+                    document = new ProDocumentWorker();
+                    break;
+                case "exp":
+                    document = new ExpertDocumentWorker();
+                    break;
+                case "":
+                    document = new DocumentWorker();
+                    break;
+                default:
+                    break;
+            }
+
+            if (key == "pro")
+            {
+                document.EditDocument();
+                document.SaveDocument();
+            }
+            else if (key == "exp")
+            {
+                document.SaveDocument();
+            }
+            else
+            {
+                document.OpenDocument();
+                document.EditDocument();
+                document.SaveDocument();
+            }
+        }
+    }
+
+    class DocumentWorker
+    {
+        public virtual void OpenDocument()
+        {
+            Console.WriteLine("Документ открыт");
+        } 
+        public virtual void EditDocument()
+        {
+            Console.WriteLine("Редактирование документа доступно в версии Про");
+        } 
+        public virtual void SaveDocument()
+        {
+            Console.WriteLine("Сохранение документа доступно в версии Про");
+        } 
+    }
+
+    class ProDocumentWorker : DocumentWorker
+    {
+        /*public override void OpenDocument()
+        {
+            Console.WriteLine("Документ открыт");
+        }*/
+        public override void EditDocument()
+        {
+            Console.WriteLine("Документ отредактирован");
+        }
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт");
+        }
+    }
+
+    class ExpertDocumentWorker : ProDocumentWorker
+    {
+        /*public override void OpenDocument()
+        {
+            Console.WriteLine("Документ открыт");
+        }
+        public override void EditDocument()
+        {
+            Console.WriteLine("Документ отредактирован");
+        }*/
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в новом формате");
         }
     }
     
